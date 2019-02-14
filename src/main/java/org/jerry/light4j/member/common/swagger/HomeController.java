4202f17504c1646e08da20f2db0a21d033522ebd
jian <janine.jian.chen@gmail.com>
2019-02-14 11:36:14 +0800

    update annotions

--- src/main/java/org/jerry/light4j/member/common/swagger/HomeController.java
+++ src/main/java/org/jerry/light4j/member/common/swagger/HomeController.java
@@ -15,4 +15,5 @@ public class HomeController {
 	public String index() {
 		return "redirect:swagger-ui.html";
 	}
+	
 }
--- src/main/java/org/jerry/light4j/member/etm/member/user/controller/MemberUserControllor.java
+++ src/main/java/org/jerry/light4j/member/etm/member/user/controller/MemberUserControllor.java
@@ -3,41 +3,49 @@ package org.jerry.light4j.member.etm.member.user.controller;
 import java.io.Serializable;
 
 import org.jerry.light4j.member.common.base.repository.impl.BaseQueryRepositoryImpl;
-import org.jerry.light4j.member.etm.member.user.api.MemberUserApi;
 import org.jerry.light4j.member.etm.member.user.domain.MemberUser;
 import org.jerry.light4j.member.etm.member.user.service.MemberUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-
-
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
-@Scope("prototype")
-public class MemberUserCotrollor implements MemberUserApi{
+@Api(tags="member.user",value="用户模块")
+@RequestMapping(value = "/member/user/",produces = { "application/json; charset=UTF-8" })
+public class MemberUserControllor{
     @Autowired
 	private MemberUserService memberUserService;
     @Autowired
     private BaseQueryRepositoryImpl<MemberUser, Serializable> baseQueryRepositoryImpl;
     
-    @Override
+    @ApiOperation(value = "新增用户")
+	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<?> save(
 			@ApiParam(value = "实体数据", required = true) @RequestBody MemberUser memberUser) {
     	memberUserService.save(memberUser);
 		return new ResponseEntity<MemberUser>(HttpStatus.OK);
 	}
-	@Override
+	
+    @ApiOperation(value = "删除用户")
+	@RequestMapping(method = RequestMethod.DELETE)
 	public ResponseEntity<?> delete(
 			@ApiParam(value = "实体数据", required = true) @RequestBody MemberUser memberUser) {
 		memberUserService.delete(memberUser);
 		return new ResponseEntity<MemberUser>(HttpStatus.OK);
 	}
-	@Override
+	
+    @ApiOperation(value = "实体修改")
+	@RequestMapping(method = RequestMethod.PUT)
 	public ResponseEntity<?> update(
 			@ApiParam(value = "实体数据", required = true) @RequestBody MemberUser memberUser) {
 		memberUserService.update(memberUser);
