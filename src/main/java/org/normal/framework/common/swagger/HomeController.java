fe027cf48ef5f3b627d8e0388aa43c882d9e6f14
jian <janine.jian.chen@gmail.com>
2019-07-31 15:48:45 +0800

    add webapp floder to application

--- src/main/java/org/normal/framework/common/swagger/HomeController.java
+++ src/main/java/org/normal/framework/common/swagger/HomeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 	
-	@GetMapping(value = "/")
+	@GetMapping(value = "/login")
 	public String index() {
 		return "redirect:login.html";
 	}
